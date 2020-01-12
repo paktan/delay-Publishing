85de0ac9f2f7fbeaa1d2e48bfd966dce46443f35
paktan <paktan.jyp@navercorp.com>
2020-01-12 15:00:51 +0000

    delay shop deploy

---
+++ precache-manifest.3409c7bb31f8d7185d64a2e70ae83329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cf967a0b68a23ef8a4109b12019b64",
+    "url": "/paktan.github.io/delay-shop/index.html"
+  },
+  {
+    "revision": "4fd4418616d6ac0cbf15",
+    "url": "/paktan.github.io/delay-shop/static/css/main.eabff5f8.chunk.css"
+  },
+  {
+    "revision": "dd9c29bbd71bb0eadcb6",
+    "url": "/paktan.github.io/delay-shop/static/js/2.fa83e4b4.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/paktan.github.io/delay-shop/static/js/2.fa83e4b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fd4418616d6ac0cbf15",
+    "url": "/paktan.github.io/delay-shop/static/js/main.13d4d087.chunk.js"
+  },
+  {
+    "revision": "5f4a4e90342b6a4e25e6",
+    "url": "/paktan.github.io/delay-shop/static/js/runtime-main.55b1b274.js"
+  }
+]);
